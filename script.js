43348d7b9947e19acc5b47b0620b5ac2c919a797
FatmaNageh <146003045+FatmaNageh@users.noreply.github.com>
2025-04-05 19:19:24 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Simple script to handle search
-document.querySelector('button').addEventListener('click', () => {
-    const input = document.querySelector('input').value;
-    alert(`Searching for "${input}"...`);
-  });
-  // script.js
-// Optional functionality
-console.log("Templates page loaded.");
