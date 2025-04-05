f6ebc1bd4ca26c4a575de6057f033398a0763317
Mariam Ashraf <135599579+mariammhassann@users.noreply.github.com>
2025-04-05 19:36:54 +0200

    Add files via upload

---
+++ RTM/demo/src/main/resources/static/js/script.js
@@ -0,0 +1,8 @@
+// Simple script to handle search
+document.querySelector('button').addEventListener('click', () => {
+    const input = document.querySelector('input').value;
+    alert(`Searching for "${input}"...`);
+  });
+  // script.js
+// Optional functionality
+console.log("Templates page loaded.");
